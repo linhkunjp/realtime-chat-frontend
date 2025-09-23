5a777c1675be1e28fb2ad37d1f4bbe9d5783f71b
linhkunjp <ktzjin@gmail.com>
2025-09-23 13:18:30 +0700

    update: deploy vercel

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,5 @@
 export const CONSTANTS = Object.freeze({
-  CHAT_API_V1: "http://localhost:5000/v1/",
+  // CHAT_API_V1: "http://localhost:5000/v1/",
+  CHAT_API_V1: "https://realtime-chat-backend-gz4h.onrender.com/v1/",
+
 });
--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000");
+const socket = io("https://realtime-chat-backend-gz4h.onrender.com");
 // const socket = '';
 
 export default socket;
